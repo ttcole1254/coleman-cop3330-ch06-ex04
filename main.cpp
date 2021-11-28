31cbbe9fdf7e220dac223a1578ee5a21e7317e8b
Test <Test@test.com>
2021-11-27 22:29:25 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Tyler Coleman
+ */
+
+/*
+* Define a class Name_value that holds a string and a value. Rework exercise 19 in Chapter 4 to use a
+* vector<Name_value> instead of two vectors.
+
+* Write a program where you first enter a set of name-and-value pairs, such as Joe 17 and Barbara 22. For each pair, add
+* the name to a vector called names and the number to a vector called scores (in corresponding positions, so that if
+* names[7]=="Joe" then scores[7]==17). Terminate input with NoName 0. Check that each name is unique and
+* terminate with an error message if a name is entered twice. Write out all the (name,score) pairs, one per line.
+*/
